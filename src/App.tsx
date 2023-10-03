5491e24082aed067cc19e915e264815e10d898d9
olvalaskevich <olvol269@gmail.com>
2023-10-03 15:20:13 +0300

    add areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header";
-import {Technologies} from "./components/Technologies";
 
 function App() {
   return (
-    <div>
-        <Header/>
-        <Technologies/>
+    <div className='app-wrapper'>
+       <header className='header'>
+           <img src={'https://avatars.mds.yandex.net/i?id=08f38c1be7061b5c61cbb97e4a8c3e17298bff6a-5364864-images-thumbs&n=13'} alt={'pict'}/>
+       </header>
+        <nav className='nav'>
+            <div><a>Profile</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Settings</a></div>
+        </nav>
+        <div className='content'>
+            <img src={'https://fikiwiki.com/uploads/posts/2022-02/1644965580_6-fikiwiki-com-p-kartinki-priroda-na-zastavku-telefona-6.jpg'} alt={'main pict'}/>
+        </div>
     </div>
   );
 }
