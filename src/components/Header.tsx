9f52e7dea4f917851aa04992f84c15110e1b2239
olvalaskevich <olvol269@gmail.com>
2023-10-04 10:09:40 +0300

    create new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src={'https://avatars.mds.yandex.net/i?id=08f38c1be7061b5c61cbb97e4a8c3e17298bff6a-5364864-images-thumbs&n=13'} alt={'pict'}/>
+        </header>
+    );
+};
+
