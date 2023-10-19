ab06151a36c1358e8141452fbfc9a0134c01910e
olvalaskevich <olvol269@gmail.com>
2023-10-19 17:13:47 +0300

    add data

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import s from './Dialogs.module.css'
+import s from './Dialogs.module.css';
+import {Names} from "./Names";
+import {Messages} from "./Messages";
+
+export type NameType={
+    id:string,
+    name:string
+}
+
+
 export const Dialogs = () => {
+    let names=[
+        {id:'1', name:'Alice'},
+        {id:'2', name:'Olga'},
+        {id:'3', name:'Anna'},
+        {id:'4', name:'Helen'},
+        {id:'5', name:'Ivan'},
+    ]
+    let messages=['Hello','How are you?']
+
+
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <Names names={names}/>
+            <Messages messages={messages}/>
         </div>
     );
 };
