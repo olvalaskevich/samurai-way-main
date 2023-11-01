a09d0320f6de7b06f87265cd0f3a09a030025a9e
olvalaskevich <olvol269@gmail.com>
2023-11-01 09:31:57 +0300

    change attribute in routes

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,11 @@ function App() {
         <Header/>
         <Navbar/>
         <div className='content'>
-            <Route path={'/profile'} component={Profile}/>
-            <Route path={'/dialogs'} component={Dialogs}/>
-            <Route path={'/news'} component={News}/>
-            <Route path={'/music'} component={Music}/>
-            <Route path={'/settings'} component={Settings}/>
+            <Route path={'/profile'} render={()=><Profile/>}/>
+            <Route path={'/dialogs'} render={()=><Dialogs/>}/>
+            <Route path={'/news'} render={()=><News/>}/>
+            <Route path={'/music'} render={()=><Music/>}/>
+            <Route path={'/settings'} render={()=><Settings/>}/>
         </div>
 
     </div>
