1eb65c6ac76b5c489542faf7231ecf36b885c5a6
olvalaskevich <olvol269@gmail.com>
2023-11-01 09:32:54 +0300

    add onClick

--- src/components/Dialogs/Names.tsx
+++ src/components/Dialogs/Names.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import s from "./Dialogs.module.css";
 import {NavLink} from "react-router-dom";
 import {NameType} from "./Dialogs";
-import {message} from "antd";
+
 
 type NamesPropsType={
     names:Array<NameType>
+    onClickChecked:(id:string)=>void
 }
 
 export const Names = (props:NamesPropsType) => {
     return (
         <ul className={s.name}>
             {props.names.map((t)=>{
-                return <li><NavLink to={'/dialogs/'+ t.id} activeClassName={s.active} className={s.message}>{t.name}</NavLink></li>
+                return <li><NavLink onClick={()=>{props.onClickChecked(t.id)}} to={'/dialogs/'+ t.id} activeClassName={s.active} className={s.message}>{t.name}</NavLink></li>
             })}
 
         </ul>
