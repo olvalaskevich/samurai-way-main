6c44c53af46c3e8edad58630157d004efa83ff9e
olvalaskevich <olvol269@gmail.com>
2023-11-04 22:39:23 +0300

    add textarea for every name

--- src/components/Dialogs/Messages.tsx
+++ src/components/Dialogs/Messages.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Dialogs.module.css";
-import {messagesType} from "./Dialogs";
+import {messagesType} from "../../App";
 
 type MessagesPropsType={
-    messages: Array<string>
+    messages: messagesType
+    idN:string
+    addMessage:(value:string, id:string)=>void
+
 }
 
 export const Messages = (props:MessagesPropsType) => {
+
+    let [value, setValue]=useState('')
+
+    const addMessageHandler=()=>{
+        props.addMessage(value, props.idN)
+        setValue('')
+    }
+
     return (
+        <>
         <ul className={s.text}>
-            {props.messages.map((t)=>{
+
+            {props.messages[props.idN].map((t)=>{
                 return <li>{t}</li>
             })}
+            <textarea value={value} onChange={(e)=>{setValue(e.currentTarget.value)}}></textarea>
+            <button onClick={addMessageHandler}>Send</button>
         </ul>
+
+        </>
     );
 };
 
