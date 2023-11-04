ea040112ee6b971d9683e2981a00037cd9b8e7f7
olvalaskevich <olvol269@gmail.com>
2023-11-04 22:25:29 +0300

    change structure of code of component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,48 +1,42 @@
-import React, {useState} from 'react';
+import React from 'react';
 import s from './Dialogs.module.css';
-import {Names} from "./Names";
 import {Messages} from "./Messages";
+import {messagesType, NameType} from "../../App";
+import {NavLink} from "react-router-dom";
 
-export type NameType={
-    id:string,
-    name:string
+
+
+type DialogsPropsType={
+    sendMessages:messagesType,
+    names:Array<NameType>,
+    onClickChecked:(id:string)=>void
+    addMessage:(value:string, id:string)=>void
 }
-export type messagesType={
-    [key:string]:Array<string>
-}
 
 
-export const Dialogs = () => {
-
-    let [sendMessages, setSendMessages]=useState<Array<string>>([])
-
-    let names=[
-        {id:'1', name:'Alice'},
-        {id:'2', name:'Olga'},
-        {id:'3', name:'Anna'},
-        {id:'4', name:'Helen'},
-        {id:'5', name:'Ivan'},
-    ]
-    let messages:messagesType={
-        '1': ['Hello','How are you 1 ?'],
-        '2': ['Hello','How are you 2 ?'],
-        '3': ['Hello','How are you 3 ?'],
-        '4': ['Hello','How are you 4 ?'],
-        '5': ['Hello','How are you 5 ?'],
-
-    }
-
-    const onClickChecked=(id:string)=>{
-        let checkedMessages=messages[id]
-        setSendMessages(checkedMessages)
-    }
-
+export const Dialogs = (props:DialogsPropsType) => {
 
 
     return (
         <div className={s.dialogs}>
-            <Names names={names} onClickChecked={onClickChecked}/>
-            <Messages messages={sendMessages}/>
+            <ul className={s.name}>
+                {props.names.map((t)=>{
+                    const onClickCheckedHandler=()=>{
+                        props.onClickChecked(t.id)
+                    }
+
+
+                    return <li><NavLink onClick={onClickCheckedHandler}
+                                        to={'/dialogs/' + t.id}
+                                        activeClassName={t.isActive? s.active:''}
+                                        className={s.message}>{t.name}
+                    </NavLink>
+                        {t.isActive && <Messages idN={t.id} messages={props.sendMessages} addMessage={props.addMessage} />}
+                    </li>
+                })}
+
+            </ul>
+
         </div>
     );
 };
