884dfa698a833e24ebbc031d07f4d761186b1e76
olvalaskevich <olvol269@gmail.com>
2023-12-12 11:04:39 +0300

    create code of users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import u from './users.module.css'
+import {useDispatch, useSelector} from "react-redux";
+import {RootStateType} from "../../state/store";
+import {followAC} from "../../state/users-reducer";
+
+type addressType={
+    city:string
+    street:string
+}
+
+export type userType={
+    id:number
+    follow:boolean
+    name:string
+    description:string
+    address: addressType
+}
 
 export const Users = () => {
+    let users=useSelector<RootStateType, Array<userType>>((state)=>state.users as Array<userType>)
+    let dispatch=useDispatch()
+
     return (
-        <div className={u.wrapper}>
-            <div className={u.ava}>
-            <img src={'https://avatars.mds.yandex.net/i?id=77768f59f45a292ee29cdab856d5adc77a38e363-4231455-images-thumbs&n=13'} alt={'ava'}/>
-            <button>Follow</button>
-            </div>
-            <div className={u.container}>
-                <div className={u.name}>
-                    <span>NAME</span>
-                    <div>DESCRIPTION</div>
+
+<>
+    {users.map((user)=>{
+            return <div className={u.wrapper}>
+                <div className={u.ava}>
+                    <img src={'https://avatars.mds.yandex.net/i?id=77768f59f45a292ee29cdab856d5adc77a38e363-4231455-images-thumbs&n=13'} alt={'ava'}/>
+                    <button onClick={()=>{dispatch(followAC(user.id))}}>
+                        {user.follow? 'UNFOLLOW':'FOLLOW'}
+                    </button>
                 </div>
-                <span>ADDRESS</span>
+                <div className={u.container}>
+                    <div className={u.name}>
+                        <span>NAME</span>
+                        <div>DESCRIPTION</div>
+                    </div>
+                    <span>{user.address.city+','+ user.address.street}</span>
+                </div>
             </div>
-        </div>
+        })}
+</>
     );
+
+
 };
