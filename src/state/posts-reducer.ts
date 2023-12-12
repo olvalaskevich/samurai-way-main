bbb428705e6a80957f6854e2e6a2d096a1072e0e
olvalaskevich <olvol269@gmail.com>
2023-12-12 11:03:26 +0300

    add initial state of posts

--- src/state/posts-reducer.ts
+++ src/state/posts-reducer.ts
@@ -4,10 +4,15 @@ type ActionAddType={
     value:string
 }
 
-export const postsReducer=(state:Array<string>, action:ActionAddType)=>{
+let initialState=[
+    'Alice Yo', 'Olga WOW', 'Anna HEY', 'Helen MAM', 'Ivan DAD'
+]
+
+export const postsReducer=(state:Array<string>=initialState, action:ActionAddType)=>{
     switch (action.type){
         case 'Add new post':
             return [action.value, ...state]
+        default : return state
     }
 }
 
