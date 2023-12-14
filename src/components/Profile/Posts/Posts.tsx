dee1dd5ad5cf9d9a6a3b5bfa097f95bd039001c2
olvalaskevich <olvol269@gmail.com>
2023-12-14 12:38:12 +0300

    add callbacks

--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -1,29 +1,36 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {Post} from "../Post/Post";
 import {Btn} from "../../Button/Btn";
+import {useDispatch, useSelector} from "react-redux";
+import {RootStateType} from "../../../state/store";
+import {postAC} from "../../../state/posts-reducer";
 
 
 
 
 export const Posts = () => {
 
-    let [allPosts, setSendMessages]=useState<Array<string>>([
-        'Alice Yo','Olga WOW','Anna HEY','Helen MAM','Ivan DAD'
-    ])
+    let dispatch=useDispatch()
+
+    let allPosts=useSelector<RootStateType, Array<string>>((state)=>state.posts)
 
     let [newPost, setNewPost]=useState('')
 
-    const onChangePost=()=>{
-        setSendMessages([newPost, ...allPosts])
-        setNewPost('')
+    const onChangeTextareaHandler=(event:ChangeEvent<HTMLTextAreaElement>)=>{
+        setNewPost(event.currentTarget.value)
+
     }
 
+    const onChangePostHandler=()=>{
+        dispatch(postAC(newPost))
+        setNewPost('')
+    }
 
     return (
         <div>
             <h1>My posts</h1>
-            <textarea value={newPost} onChange={(event)=>{setNewPost(event.currentTarget.value)}}></textarea>
-            <Btn title='Send' onChangePost={onChangePost}/>
+            <textarea value={newPost} onChange={onChangeTextareaHandler}></textarea>
+            <Btn title='Send' onChangePost={onChangePostHandler}/>
             <Post text={allPosts}/>
 
         </div>
