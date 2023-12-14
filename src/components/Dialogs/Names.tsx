5971b465a684984882f0da3bafd96cb70598533d
olvalaskevich <olvol269@gmail.com>
2023-12-14 12:37:47 +0300

    add new component

--- src/components/Dialogs/Names.tsx
+++ src/components/Dialogs/Names.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import s from "./Dialogs.module.css";
 import {NavLink} from "react-router-dom";
-import {NameType} from "./Dialogs";
+import {NameType} from "../../App";
 
 
-type NamesPropsType={
-    names:Array<NameType>
-    onClickChecked:(id:string)=>void
-}
 
-export const Names = (props:NamesPropsType) => {
-    return (
-        <ul className={s.name}>
-            {props.names.map((t)=>{
-                return <li><NavLink onClick={()=>{props.onClickChecked(t.id)}} to={'/dialogs/'+ t.id} activeClassName={s.active} className={s.message}>{t.name}</NavLink></li>
-            })}
 
-        </ul>
-    );
+
+
+export const Names = () => {
+
+
+
+
 };
 
