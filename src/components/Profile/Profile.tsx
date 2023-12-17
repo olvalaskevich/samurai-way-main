98e6cd68c052ca2aac7fdac6eae1fedc00046f78
olvalaskevich <olvol269@gmail.com>
2023-12-17 18:16:44 +0300

    change code

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,6 +3,8 @@ import s from './Profile.module.css'
 import {Posts} from "./Posts/Posts";
 import {MyProfile} from "./Posts/MyProfile";
 
+
+
 export const Profile = () => {
     return (
         <div className={s.content}>
