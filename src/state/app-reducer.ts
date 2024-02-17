c99c64753c17ff562b52e578a049b6b993fe644f
olvalaskevich <olvol269@gmail.com>
2024-02-17 11:17:38 +0300

    create reducer for errors and loading status

---
+++ src/state/app-reducer.ts
@@ -0,0 +1,25 @@
+
+
+export type StatusType='idle'|'loading'|'success'
+
+type AppStatusType= {
+    status:StatusType
+}
+let initialState:AppStatusType={
+    status:'idle'
+}
+
+export type ActionAppStatusType=ReturnType<typeof setStatusAC>
+
+export const appReducer=(state:AppStatusType=initialState, action:ActionAppStatusType)=>{
+    switch (action.type){
+        case 'SET-STATUS':
+            return {...state, status: action.status}
+        default:
+            return state
+    }
+}
+
+export const setStatusAC=(status:StatusType)=>{
+    return ({type:'SET-STATUS', status: status} as const)
+}
