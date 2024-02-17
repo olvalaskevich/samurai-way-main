c2f476f68a340624679b4832de11cdad296e7346
olvalaskevich <olvol269@gmail.com>
2024-02-17 11:19:43 +0300

    add reducer and action type

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,18 +1,24 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
-import {postsReducer} from "./posts-reducer";
+
 import {messagesReducer} from "./messages-reducer";
-import {usersReducer} from "./users-reducer";
-import {thunk} from "redux-thunk";
+import {GeneralActionType, usersReducer} from "./users-reducer";
+import {thunk, ThunkAction, ThunkDispatch} from "redux-thunk";
+import {ActionAppStatusType, appReducer} from "./app-reducer";
+import {ActionProfileType, profileReducer} from "./profile-reducer";
 
 
 
 const rootReducer=combineReducers({
-    posts: postsReducer,
+    profile: profileReducer,
     messages: messagesReducer,
-    users: usersReducer
+    users: usersReducer,
+    app: appReducer
 
 })
 
 export type RootStateType=ReturnType<typeof rootReducer>
+export type RootActionType=ActionAppStatusType | GeneralActionType | ActionProfileType
+export type ThunkActionCreatorType=ThunkAction<void, RootStateType, unknown, RootActionType>
+export type DispatchActionType=ThunkDispatch<RootStateType, unknown, RootActionType>
 
 export const store=createStore(rootReducer, applyMiddleware(thunk))
