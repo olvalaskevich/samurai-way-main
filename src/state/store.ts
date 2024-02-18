24c39fb896465ecd633e8fad439c7e1307a4eeeb
olvalaskevich <olvol269@gmail.com>
2024-02-18 20:52:19 +0300

    add reducer for auth

--- src/state/store.ts
+++ src/state/store.ts
@@ -5,6 +5,7 @@ import {GeneralActionType, usersReducer} from "./users-reducer";
 import {thunk, ThunkAction, ThunkDispatch} from "redux-thunk";
 import {ActionAppStatusType, appReducer} from "./app-reducer";
 import {ActionProfileType, profileReducer} from "./profile-reducer";
+import {AuthActionsType, authReducer} from "./auth-reducer";
 
 
 
@@ -12,12 +13,13 @@ const rootReducer=combineReducers({
     profile: profileReducer,
     messages: messagesReducer,
     users: usersReducer,
-    app: appReducer
+    app: appReducer,
+    auth:authReducer
 
 })
 
 export type RootStateType=ReturnType<typeof rootReducer>
-export type RootActionType=ActionAppStatusType | GeneralActionType | ActionProfileType
+export type RootActionType=ActionAppStatusType | GeneralActionType | ActionProfileType | AuthActionsType
 export type ThunkActionCreatorType=ThunkAction<void, RootStateType, unknown, RootActionType>
 export type DispatchActionType=ThunkDispatch<RootStateType, unknown, RootActionType>
 
