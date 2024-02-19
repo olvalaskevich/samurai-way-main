06212bb6468a2eaea7ed8e12f0340468f3c222f6
olvalaskevich <olvol269@gmail.com>
2024-02-19 13:51:06 +0300

    add disabled for button

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,13 @@
 import React, {MouseEventHandler, useEffect, useState} from 'react';
 import u from './users.module.css'
 import {useDispatch, useSelector} from "react-redux";
-import {RootStateType} from "../../state/store";
-import {FollowAC, GetUsersTC} from "../../state/users-reducer";
+import {DispatchActionType, RootActionType, RootStateType, ThunkActionCreatorType} from "../../state/store";
+import {FollowAC, GetUsersTC, SetFollowTC} from "../../state/users-reducer";
 import '../../index.css'
+import {StatusType} from "../../state/app-reducer";
+import {CircularProgress} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import {setCheckedUserTC} from "../../state/profile-reducer";
 
 
 export type userType={
@@ -17,19 +21,27 @@ export type userType={
     followed: boolean
 }
 
-export type UserStateType={
+export type UserStateResponseType={
     items:Array<userType>
     totalCount:number
     error:string
 }
 
+export type UserStateType={
+    items:Array<userType>
+    totalCount:number
+    error:string
+} & {countPage:number} & {status:StatusType}
+
 export const Users = () => {
-    let dispatch = useDispatch()
+    let dispatch = useDispatch<DispatchActionType>()
+    let count=useSelector<RootStateType, number>((state)=>state.users.countPage)
     useEffect(() => {
-        dispatch(GetUsersTC(1))
+        dispatch(GetUsersTC(count, 1))
     }, []);
 
-    let users = useSelector<RootStateType, UserStateType>((state) => state.users as UserStateType)
+    let users = useSelector<RootStateType, UserStateType>((state) => state.users)
+    let loading=useSelector<RootStateType,StatusType>((state)=>state.users.status)
     let [activePage, setActive] = useState(1)
     let countOfPages = users.totalCount % 10 === 0 ? users.totalCount / 10 : (users.totalCount / 10) + 1
     let resPages = countOfPages > 10 ? 10 : countOfPages
@@ -39,21 +51,27 @@ export const Users = () => {
     }
 
 
+
     return (
 
 <>
     {pages.map((p)=>{
         return <span className={activePage===p?'active':''} onClick={()=>{
+            dispatch(GetUsersTC(count, p))
             setActive(p)
-            dispatch(GetUsersTC(p))
         }}>{p}</span>})
     }
-
+    <div>
+        {loading === 'loading' && <CircularProgress/>}
+    </div>
     {users.items.map((user)=>{
+
             return <div className={u.wrapper}>
                 <div className={u.ava}>
-                    <img src={user.photos.large} alt={'ava'}/>
-                    <button onClick={()=>{dispatch(FollowAC(user.id))}}>
+                    <NavLink onClick={()=>{dispatch(setCheckedUserTC(user.id))}} to={'/users/usersprofile/'+user.id}>
+                        <img src={user.photos.small} alt={'ava'}/>
+                    </NavLink>
+                    <button disabled={user.status==='loading'} onClick={()=>{dispatch(SetFollowTC(user.id))}}>
                         {user.followed? 'UNFOLLOW':'FOLLOW'}
                     </button>
                 </div>
