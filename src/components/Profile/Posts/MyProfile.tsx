887977012b4d1f5628760ee5c6886c2938997952
olvalaskevich <olvol269@gmail.com>
2024-03-14 11:27:15 +0300

    add login and editable span for status

--- src/components/Profile/Posts/MyProfile.tsx
+++ src/components/Profile/Posts/MyProfile.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from "../Profile.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {DispatchActionType, RootStateType} from "../../../state/store";
+import {Redirect} from "react-router-dom";
+import {EditSpan} from "../EditSpan";
+import {changeStatusTC, getStatusTC} from "../../../state/profile-reducer";
 
 export const MyProfile = () => {
+    let isAuth=  useSelector<RootStateType, boolean>((state) => state.auth.isAuth)
+    let myUserId=useSelector<RootStateType, number|null>((state) => state.auth.authData.id)
+
+    let editSpanTitle=  useSelector<RootStateType, string>((state) => state.profile.status)
+
+    let dispatch=useDispatch<DispatchActionType>()
+    const changeEditSpan=(value:string)=>{
+        dispatch(changeStatusTC(value))
+    }
+    useEffect(() => {
+        dispatch(getStatusTC(myUserId))
+    }, []);
+    if (!isAuth) return <Redirect to={'/login'}/>
     return (
         <div>
             <img
@@ -10,7 +28,7 @@ export const MyProfile = () => {
             <div className={s.photo}>
                 <img src={"https://klike.net/uploads/posts/2023-01/1673594987_3-24.jpg"} alt={'photo'}/>
             </div>
-            <div>text</div>
+            <EditSpan title={editSpanTitle} changeEditSpan={changeEditSpan}/>
         </div>
     );
 };
