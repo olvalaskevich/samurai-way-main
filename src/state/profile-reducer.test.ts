2c55074e4c0c647d609bad8a8d6441158accf77b
olvalaskevich <olvol269@gmail.com>
2024-05-22 22:15:02 +0300

    add tests for profile reducer

---
+++ src/state/profile-reducer.test.ts
@@ -0,0 +1,77 @@
+import {addPostAC, changeStatusAC, getStatusAC, profileReducer, setUserProfileAC} from "./profile-reducer";
+import {logOutAC} from "./auth-reducer";
+
+let testState={
+    profile: {
+        userId: null,
+        lookingForAJob: false,
+        lookingForAJobDescription: '',
+        fullName: '',
+        contacts: {
+            github: '',
+            vk: '',
+            facebook: '',
+            instagram: '',
+            twitter: '',
+            website: '',
+            youtube: '',
+            mainLink: '',
+        },
+        photos: {
+            small: '',
+            large: ''
+        }
+    },
+    posts:[
+        {id:1, message:'Hello every people', likesCount:0},
+        {id:2, message:'Hi every people', likesCount:0}
+    ],
+    newPostText:'',
+    status: '---'
+}
+test('users profile should be added', ()=>{
+
+    let resultState=profileReducer(testState, setUserProfileAC({
+        userId: 1,
+        lookingForAJob: true,
+        lookingForAJobDescription: 'string',
+        fullName: "string",
+        contacts: {
+            github: "string",
+            vk: "string",
+            facebook: "string",
+            instagram: "string",
+            twitter: "string",
+            website: "string",
+            youtube: "string",
+            mainLink: "string",
+        },
+        photos: {
+            small: "string",
+            large: "string",
+        }
+    }))
+
+    expect(resultState.profile.userId).toBe(1)
+})
+
+test('post should be added', ()=>{
+
+    let resultState=profileReducer(testState, addPostAC('Hello'))
+
+    expect(resultState.posts[2].message).toBe('Hello')
+})
+
+test('status should be changed', ()=>{
+
+    let resultState=profileReducer(testState, changeStatusAC('Search offer'))
+
+    expect(resultState.status).toBe('Search offer')
+})
+
+test('should be logout', ()=>{
+
+    let resultState=profileReducer(testState, logOutAC())
+
+    expect(resultState.status).toBe('---')
+})
