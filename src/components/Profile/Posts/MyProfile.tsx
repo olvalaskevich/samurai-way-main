9306e33d8a9f5fda422dd9a7910c85c98d2eef38
olvalaskevich <olvol269@gmail.com>
2024-06-13 22:08:56 +0300

    add code for update photo in useEffect

--- src/components/Profile/Posts/MyProfile.tsx
+++ src/components/Profile/Posts/MyProfile.tsx
@@ -1,25 +1,39 @@
-import React, {useEffect} from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
 import s from "../Profile.module.css";
 import {useDispatch, useSelector} from "react-redux";
 import {DispatchActionType, RootStateType} from "../../../state/store";
 import {Redirect} from "react-router-dom";
 import {EditSpan} from "../EditSpan";
-import {changeStatusTC, getStatusTC} from "../../../state/profile-reducer";
+import {
+    ChangeProfilePhotoTC,
+    changeStatusTC,
+    getStatusTC,
+    ProfileStateType,
+    setCheckedUserTC
+} from "../../../state/profile-reducer";
 import img from '../../../assets/i.webp'
 
 export const MyProfile = () => {
     let isAuth=  useSelector<RootStateType, boolean>((state) => state.auth.isAuth)
     let myUserId=useSelector<RootStateType, number|null>((state) => state.auth.authData.id)
-
     let editSpanTitle=  useSelector<RootStateType, string>((state) => state.profile.status)
+    let photo=useSelector<RootStateType, string>((state) => state.profile.profile.photos.small)
 
     let dispatch=useDispatch<DispatchActionType>()
     const changeEditSpan=(value:string)=>{
         dispatch(changeStatusTC(value))
     }
     useEffect(() => {
+        dispatch(setCheckedUserTC(myUserId))
         dispatch(getStatusTC(myUserId))
     }, []);
+    const ChangeFileHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+        if (e.target.files?.length){
+            dispatch(ChangeProfilePhotoTC(e.target.files[0]))
+        }
+
+    }
+
     if (!isAuth) return <Redirect to={'/login'}/>
     return (
         <div>
@@ -27,9 +41,13 @@ export const MyProfile = () => {
                 src={'https://fikiwiki.com/uploads/posts/2022-02/1644965580_6-fikiwiki-com-p-kartinki-priroda-na-zastavku-telefona-6.jpg'}
                 alt={'main pict'}/>
             <div className={s.photo}>
-                <img src={img} alt={'photo'}/>
+                <img src={photo} alt={'photo'}/>
             </div>
-            <EditSpan title={editSpanTitle} changeEditSpan={changeEditSpan}/>
+            <input type={"file"} onChange={(e)=>ChangeFileHandler(e)}></input>
+            <div>
+                <EditSpan title={editSpanTitle} changeEditSpan={changeEditSpan}/>
+            </div>
+
         </div>
     );
 };
