af0c5e32e7d582142d93ea89c839839c18d5812c
olvalaskevich <olvol269@gmail.com>
2024-06-17 20:18:11 +0300

    add new component for profile form

---
+++ src/components/Profile/ProfileForm.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {useFormik} from "formik";
+import {RootStateType} from "../../state/store";
+import {Redirect} from "react-router-dom";
+import {setLoginTC} from "../../state/auth-reducer";
+import {ProfileStateType} from "../../state/profile-reducer";
+
+type ProfileFormErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+export const ProfileForm = () => {
+    let dispatch=useDispatch()
+    let profileInfo=useSelector<RootStateType,ProfileStateType>((state)=>state.profile)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: ProfileFormErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            if (!values.password){
+                errors.password = 'Required'
+            }
+            return errors
+        },
+        onSubmit: values => {
+            dispatch(setLoginTC(values))
+
+        },
+    })
+          return (
+            <Grid container justifyContent={'center'}>
+                <Grid item justifyContent={'center'}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <FormControl>
+                            <FormLabel>
+                                <p>To log in get registered
+                                    <a href={'https://social-network.samuraijs.com/'}
+                                       target={'_blank'}> here
+                                    </a>
+                                </p>
+                                <p>or use common test account credentials:</p>
+                                <p>Email: free@samuraijs.com</p>
+                                <p>Password: free</p>
+                            </FormLabel>
+                            <FormGroup>
+                                <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}/>
+                                {formik.touched.email && formik.errors.email && <div style={{color:'red'}}>{formik.errors.email}</div>}
+
+                                <TextField type="password" label="Password" margin="normal" {...formik.getFieldProps('password')}/>
+                                {formik.touched.password && formik.errors.password && <div style={{color:'red'}}>{formik.errors.password}</div>}
+
+                                <FormControlLabel label={'Remember me'} control={<Checkbox checked={formik.values.rememberMe} {...formik.getFieldProps('rememberMe')}/>}/>
+
+                                <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                    Login
+                                </Button>
+
+                            </FormGroup>
+                        </FormControl>
+                    </form>
+                </Grid>
+            </Grid>
+        )
+}
+
