bc6b139744262a77955c9d733242c4f4219047f3
olvalaskevich <olvol269@gmail.com>
2024-06-20 19:24:31 +0300

    add code for add and close form for profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Profile.module.css'
 import {Posts} from "./Posts/Posts";
 import {MyProfile} from "./Posts/MyProfile";
+import {ProfileForm} from "./ProfileForm";
 
 
 
 
 const Profile = () => {
 
+    let [mode, setMode]=useState(true)
+    const сhangeProfile=()=>{
+        setMode(false)
+    }
+    const closeForm=()=>{
+        setMode(true)
+    }
+    if (!mode) return <ProfileForm closeForm={closeForm}/>
     return (
         <div className={s.content}>
-            <MyProfile/>
+            <MyProfile сhangeProfile={сhangeProfile}/>
             <Posts/>
         </div>
     );
