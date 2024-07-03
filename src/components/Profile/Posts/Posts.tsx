73f3a1a1f78c285a7490c3408114b262bfe11b74
olvalaskevich <olvol269@gmail.com>
2024-07-03 13:16:52 +0300

    add new code for work button my posts

--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -4,6 +4,7 @@ import {Btn} from "../../Button/Btn";
 import {useDispatch, useSelector} from "react-redux";
 import {RootStateType} from "../../../state/store";
 import {addPostAC, ProfileStateType} from "../../../state/profile-reducer";
+import s from "../Profile.module.css";
 
 
 
@@ -12,28 +13,27 @@ import {addPostAC, ProfileStateType} from "../../../state/profile-reducer";
 export const Posts = () => {
 
     let dispatch=useDispatch()
-
     let profile=useSelector<RootStateType, ProfileStateType>((state)=>state.profile)
     let allPosts=profile.posts
     let [newPost, setNewPost]=useState('')
+    let [isOpen, setIsOpen]=useState(false)
 
     const onChangeTextareaHandler=(event:ChangeEvent<HTMLTextAreaElement>)=>{
         setNewPost(event.currentTarget.value)
-
     }
-
     const onChangePostHandler=()=>{
         dispatch(addPostAC(newPost))
         setNewPost('')
     }
 
     return (
-        <div>
-            <h1>My posts</h1>
-            <textarea value={newPost} onChange={onChangeTextareaHandler}></textarea>
+        <div className={s.posts}>
+            <h1 onClick={()=>setIsOpen(!isOpen)}>My posts {isOpen?'👆':'👇'}</h1>
+            {isOpen && <Post text={allPosts}/>}
+            <div className={s.area}>
+            <textarea value={newPost} onChange={onChangeTextareaHandler} placeholder={'Enter new post'}></textarea>
             <Btn title='Send' onChangePost={onChangePostHandler}/>
-            <Post text={allPosts}/>
-
+            </div>
         </div>
     );
 };
