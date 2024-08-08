d445445818371b1647143e6e3c7ea8b09db9288a
olvalaskevich <olvol269@gmail.com>
2024-08-08 21:46:28 +0300

    add styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import s from './Profile.module.css'
-import {Posts} from "./Posts/Posts";
+import s from './Profile.module.css';
 import {MyProfile} from "./Posts/MyProfile";
 import {ProfileForm} from "./ProfileForm";
 
@@ -14,14 +13,14 @@ const Profile = () => {
         setMode(false)
     }
     const closeForm=()=>{
+
         setMode(true)
     }
 
     return (
-        <div className={s.content}>
+        <div>
             {!mode && <div className={s.form}><ProfileForm closeForm={closeForm}/></div>}
             <MyProfile mode={mode} changeProfile={changeProfile}/>
-            <Posts/>
         </div>
     );
 };
